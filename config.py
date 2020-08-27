70a7e2e9b9de2d7151a468d2c52b16856176f31a
Dmitry Zhitin <sls8@ya.ru>
2020-08-27 16:07:01 +0300

    Cоздание базы данных пользователей. Миграция Alembic

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
 
-class Config(object):
+class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'mysql+pymysql://root:root@127.0.0.1:3306/mic_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
